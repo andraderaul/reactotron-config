284461d92a46dbb55ccf4bee462afd3f42a396d6
Raul Andrade <raul10.3@hotmail.com>
2020-02-16 13:45:23 -0300

    first commit
    reactotron configured

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './config/ReactotronConfig';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import TodoList from './TodoList';
+
+console.tron.log('Testando');
+
+function App() {
+  return (
+    <Provider store={store}>
+      <TodoList />
+    </Provider>
+  );
+}
+
+export default App;
